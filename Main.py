1750b50199922e1558dcd61632fb715ff6773225
sudoVenomSnake <swastikm26@gmail.com>
2023-08-31 00:24:33 +0530

    test2

--- Main.py
+++ Main.py
@@ -232,6 +232,8 @@ if start:
 
         get_legal_act_solutions = st.checkbox(label = 'Get the possible legal approaches for the legal actions mentioned in the act')
         
+        print(type(json_data))
+
         for i in json_data['petition_tree']:
             print(i)
 
